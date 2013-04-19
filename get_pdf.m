2322833294e78bef79e5f041d2a234d047b3dc52
Jack Peterson <jack@tinybike.net>
2013-04-19 12:20:55 -0700

    digamma function file

--- get_pdf.m
+++ get_pdf.m
@@ -2,7 +2,9 @@ function [x,y,freq] = get_pdf(counts)
 
 [freq,x] = hist(counts,min(counts):max(counts));
 y = freq/sum(freq);
+freq(~freq) = [];
 x(~y) = [];
 y(~y) = [];
+freq = freq';
 x = x';
 y = y';

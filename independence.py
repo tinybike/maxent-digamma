901e83421c24be6ff240943c18e6f9f18dd17056
Jack Peterson <jack@tinybike.net>
2014-05-24 10:25:35 -0700

    removed bunch dependence

--- independence.py
+++ independence.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 """
-Chi-square test for independence for three (big) histograms.
+Chi-square test for independence between multiple histograms.
 (c) Jack Peterson (jack@tinybike.net), 5/19/2014
 """
-from __future__ import division
+import os
 from scipy.stats import chi2_contingency
-from bunch import Bunch
 
 class IndependenceTest(object):
 
-    def __init__(self, datafiles):
+    def __init__(self, datafiles, path='', significance_level=0.05):
         self.categories = ('persons', 'locations', 'orgs')
+        self.path = path
         self.datafiles = datafiles
-        self.uni_x = Bunch()
-        self.uni_y = Bunch()
+        self.significance_level = significance_level
+        self.uni_x = {}
+        self.uni_y = {}
         self.con_table = []
 
     def histograms(self):
         """Load data from files and calculate histograms"""
         for category in self.categories:
             hist = {}
-            with open(self.datafiles[category]) as datafile:
+            filepath = os.path.join(path, self.datafiles[category])
+            with open(filepath) as datafile:
                 for line in datafile:
                     try:
                         data = int(line.strip())
@@ -33,9 +35,9 @@ class IndependenceTest(object):
     def contingency_table(self):
         """Contingency table: persons | locations | organizations"""
         X = []
-        X.extend(self.uni_x.persons)
-        X.extend(self.uni_x.locations)
-        X.extend(self.uni_x.orgs)
+        X.extend(self.uni_x['persons'])
+        X.extend(self.uni_x['locations'])
+        X.extend(self.uni_x['orgs'])
         X = sorted(list(set(X)))
         for x in X:
             row = [0, 0, 0]
@@ -48,17 +50,22 @@ class IndependenceTest(object):
 
     def chisquare_independence_test(self):
         """Chi-squared test: if P < 0.05, then data sets are independent"""
-        chi2, self.pval, dof, ex = chi2_contingency(self.con_table)
-        self.result = 'independent' if self.pval < 0.05 else 'not independent'
+        self.chi2, self.pval, self.dof, self.ex = chi2_contingency(self.con_table)
+        self.result = 'independent' if self.pval < self.significance_level \
+            else 'not independent'
 
 
 def main():
-    datafiles = Bunch({
-        'persons': "freq_persons.txt",
-        'locations': "freq_places.txt",
-        'orgs': "freq_orgs.txt",
-    })
-    IT = IndependenceTest(datafiles)
+    parameters = {
+        'datafiles': {
+            'persons': "freq_persons.txt",
+            'locations': "freq_places.txt",
+            'orgs': "freq_orgs.txt",
+        },
+        'path': '',
+        'significance_level': 0.05,
+    }
+    IT = IndependenceTest(**parameters)
     IT.histograms()
     IT.contingency_table()
     IT.chisquare_independence_test()
